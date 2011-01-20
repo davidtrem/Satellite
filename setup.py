a23271601843f72f26b91f473ca0a58de8f19078
David Trémouilles <david.trem@gmail.com>
2011-01-20 08:56:06 +0100

    create a setup.py file but need some refactoring

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name = 'Satellite',
+      version = '0.5.1-beta',
+      author = 'David Trémouilles',
+      author_email = 'david.trem at gmail.com',
+      url = 'http://code.google.com/p/esdanalysistools/',
+      license = 'MIT',
+      platforms = ['any'],
+      packages = ['satellite'],
+      scripts = ['satellite.py',],
+      )
