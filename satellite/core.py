19fe1c6abdb2b53c07350346fbd03b06723b7734
David Tremouilles <david.trem@gmail.com>
2011-04-22 18:22:22 +0200

    looks now as good alpha release

--- satellite/core.py
+++ satellite/core.py
@@ -37,8 +37,12 @@ from matplotlib.backends.backend_qt4agg import (
 
 from thunderstorm.thunder.importers.tools import plug_dict
 from thunderstorm.lightning.simple_plots import TLPFigure
+from thunderstorm.lightning.simple_plots import TLPOverlay
 
 from thunderstorm.lightning.pulse_observer import TLPPulsePickFigure
+from thunderstorm.istormlib.storm import Storm
+from thunderstorm.istormlib.istorm_view import View
+
 
 class MatplotlibFig(QtGui.QWidget):
     def __init__(self, parent=None):
@@ -53,6 +57,11 @@ class MatplotlibFig(QtGui.QWidget):
         self.setLayout(fig_vbox)
         self.figure = figure
 
+class TLPOverlayFig(MatplotlibFig):
+    def __init__(self, parent=None):
+        MatplotlibFig.__init__(self, parent)
+        self.fig = TLPOverlay(self.figure)
+
 
 class TlpFig(MatplotlibFig):
     def __init__(self, tlp_curve_data, title, leakage_evol=None, parent=None):
@@ -65,7 +74,7 @@ class TlpFig(MatplotlibFig):
 class PulsesPickFig(MatplotlibFig):
     def __init__(self, raw_data, title, parent=None):
         MatplotlibFig.__init__(self, parent)
-        self.fig = TLPPulsePickFigure(self.figure, raw_data)
+        self.fig = TLPPulsePickFigure(self.figure, raw_data, title)
 
 
 class ViewTab(QtGui.QTabWidget):
@@ -83,7 +92,7 @@ class ImportLoader(QtCore.QThread):
         self.importer_name = importer_name
         self.importer = plug_dict[self.importer_name]()
         self.file_ext = self.importer.file_ext
-        self.file_name = ""
+        self.file_names = ""
 
     def __call__(self, file_name):
         self.file_names = QtGui.QFileDialog.getOpenFileNames(
@@ -109,7 +118,14 @@ class MainWin(QtGui.QMainWindow):
         self.setWindowIcon(icon)
         self.resize(800, 600)
         self.view_tab = ViewTab()
+        tlp_overlay = TLPOverlayFig()
+        self.core_storm = Storm(tlp_overlay.fig)
+        self.view_tab.addTab(tlp_overlay, "TLP")
+        # pointer to single tlp and pulsepicker figure
+        self.tlpfig = None  #single tlp figure
+        self.ppfig = None   #single pulse picker figure
 
+        #initialize menu
         file_menu = self.menuBar().addMenu("&File")
         import_menu = file_menu.addMenu("&Import")
         for importer_name in plug_dict.keys():
@@ -120,10 +136,16 @@ class MainWin(QtGui.QMainWindow):
             loader.new_data_ready.connect(self.add_new_experiment)
 
         #initialize core_storm and associated QListWidget
-        core_storm = None
         core_storm_listwdgt = QtGui.QListWidget(self)
         core_storm_listwdgt.setSelectionMode(
             QtGui.QAbstractItemView.ExtendedSelection)
+        core_storm_listwdgt.setDragDropMode(
+            QtGui.QAbstractItemView.InternalMove)
+        core_storm_listwdgt.itemSelectionChanged.connect(
+            self.core_storm_selection_change)
+        core_storm_listwdgt.setSortingEnabled(True)
+        core_storm_listwdgt.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        core_storm_listwdgt.customContextMenuRequested.connect(self.list_menu)
         layout = QtGui.QHBoxLayout()
         layout.addWidget(core_storm_listwdgt)
         layout.addWidget(self.view_tab)
@@ -131,20 +153,45 @@ class MainWin(QtGui.QMainWindow):
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
         self.core_storm_listwdgt = core_storm_listwdgt
+        self.experiment_dict = {}
+
+    def list_menu(self, position):
+        item = self.core_storm_listwdgt.itemAt(position)
+        def show_pulse_pick():
+            experiment = self.experiment_dict[item]
+            self.ppfig = PulsesPickFig(experiment.raw_data, item.text())
+            self.ppfig.show()
+        def show_tlp():
+            experiment = self.experiment_dict[item]
+            self.tlpfig = TlpFig(experiment.raw_data.tlp_curve, item.text(),
+                                 experiment.raw_data.leak_evol)
+            self.tlpfig.show()
+
+        menu = QtGui.QMenu(parent=self)
+        pulse_pick_action = QtGui.QAction("Pulse pick", menu)
+        pulse_pick_action.triggered.connect(show_pulse_pick)
+        tlp_action = QtGui.QAction("Show TLP", menu)
+        tlp_action.triggered.connect(show_tlp)
+        menu.addAction(pulse_pick_action)
+        menu.addAction(tlp_action)
+        menu.exec_(self.core_storm_listwdgt.mapToGlobal(position))
 
 
     def add_new_experiment(self, experiment, file_name):
         data_name = os.path.splitext(os.path.basename(unicode(file_name)))[0]
-        view_tab = self.view_tab
-        view_tab.addTab(TlpFig(experiment.raw_data.tlp_curve,
-                               experiment.exp_name,
-                               experiment.raw_data.leak_evol),
-                        "TLP curve")
-        view_tab.addTab(PulsesPickFig(experiment.raw_data,
-                                      experiment.exp_name),
-                        "Pulses")
-        item = QtGui.QListWidgetItem(data_name, self.core_storm_listwdgt)
+        experiment.exp_name = data_name
+        self.core_storm.append(View(experiment))
+        item = QtGui.QListWidgetItem(experiment.exp_name,
+                                     self.core_storm_listwdgt)
         item.setToolTip(data_name)
+        self.experiment_dict[item] = experiment
+
+    def core_storm_selection_change(self):
+        items = self.core_storm_listwdgt.selectedItems()
+        experiment_list = []
+        for item in items:
+            experiment_list.append(self.experiment_dict[item])
+        self.core_storm.overlay_raw_tlp(experiment_list=experiment_list)
 
 
 def main():
