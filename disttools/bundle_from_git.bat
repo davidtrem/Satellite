d47aa5fe5f76ae720c5c1ce1b857f060cee1e0a9
David Trémouilles <david.trem@gmail.com>
2012-02-09 09:38:54 +0100

    update dist tools to use ESDAnalysisTools github repo

--- disttools/bundle_from_git.bat
+++ disttools/bundle_from_git.bat
@@ -2,8 +2,8 @@
 echo ------------------------- >CON
 echo Creating Satellite Bundle >CON
 rmdir satellite_bundle /s /q
-call git clone --depth 1 git://github.com/davidtrem/Satellite.git satellite_bundle
-call git clone --depth 1 git://github.com/davidtrem/ThunderStorm.git
+call git clone --depth 1 git://github.com/ESDAnalysisTools/Satellite.git satellite_bundle
+call git clone --depth 1 git://github.com/ESDAnalysisTools/ThunderStorm.git
 xcopy ThunderStorm\thunderstorm satellite_bundle\thunderstorm\ /e >NUL
 rmdir ThunderStorm /s /q
 xcopy msdll satellite_bundle >NUL
